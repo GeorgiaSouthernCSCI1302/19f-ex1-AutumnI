f437b27f2ca3e884b758888662f5277a2ece875e
ai00877 <ai00877@CEIT2553214X024.ad.georgiasouthern.edu>
2019-08-21 16:41:36 -0400

    Changed the statement from "Hello World" to "Hello my name is"

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -4,10 +4,10 @@ public class FirstClassInGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello my name is Autumn!");
-		
+		System.out.println("Autumn");
 		//add a statement to display your name in the console
 		
-		//Then commit your changes
+		//Then commit your changes  
 
 	}
 
